3d38fa28a1ffb4056164fdee84fa11a3152fc754
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-06 18:38:45 +0000

    Created Figure.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Figure from './components/Figure';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
         <Header />
+        <Figure />
         <Footer />
     </div>
   );
