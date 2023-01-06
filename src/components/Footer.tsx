903cf0a619c09db9562ae9e71eaa21e5387126ed
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-06 18:12:48 +0000

    Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Coded by Luna Smith, open saurce on Github.</div>
+    <div>Coded by <a href='https://www.lunasmithart.com/' target='_blank' rel="noreferrer"></a>Luna Smith, open saurce on <a href='https://github.com/Lu-Smith/hangman-react-typescript' target='_blank' rel="noreferrer"></a> Github.</div>
   )
 }
 
