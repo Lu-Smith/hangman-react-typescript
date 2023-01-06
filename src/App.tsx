9a1b4f3e1b86a48eaf79985e29728eee2d5df1ea
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-06 18:58:06 +0000

    Created WrongLetters.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import './App.css';
 import Figure from './components/Figure';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import WrongLetters from './components/WrongLetters';
 
 function App() {
   return (
     <div className="App">
         <Header />
         <div className="game-container">
-        <Figure />
+           <Figure />
+           <WrongLetters />
         </div>
-        
         <Footer />
     </div>
   );
