b937db46533f0d4a069ab74928153a1ba0ba1606
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-06 18:08:03 +0000

    Created Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>
-        Hello World
+        <Header />
       </h1>
     </div>
   );
