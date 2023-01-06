d2bc073bcd007f571a09bcf48c17d61bbc6822cc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-06 19:24:54 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import React, {useState} from 'react';
 import Figure from './components/Figure';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -8,7 +9,17 @@ import PopUp from './components/PopUp';
 import Word from './components/Word';
 import WrongLetters from './components/WrongLetters';
 
+const words = ['application', 'programming', 'interface', 'wizard', 'biscuits'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+
 function App() {
+
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetterw] = useState([]);
+  
   return (
     <div className="App">
         <Header />
