d60d7927450a8516f4a5f9f8ca5b712a388f01c9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-06 18:10:11 +0000

    Created Footer.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>
         <Header />
-      </h1>
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Coded by Luna Smith, open saurce on Github.</div>
+  )
+}
+
+export default Footer
