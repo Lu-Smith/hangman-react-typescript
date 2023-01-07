0a8cfe82a5ba659e863ea3670533fb24a8cbfaa0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-07 23:28:02 +0000

    Updated Figure.tsx

--- src/components/Figure.tsx
+++ src/components/Figure.tsx
@@ -17,13 +17,13 @@ const Figure = ({wrongLetters}:wrongLettersProps) => {
     */ Head /*
     {errors > 0 && <circle cx="140" cy="70" r="20" className="figure-part" />}
     */ Body /*
-    <line x1="140" y1="90" x2="140" y2="150" className="figure-part" />
+    {errors > 1 && <line x1="140" y1="90" x2="140" y2="150" className="figure-part" />}
     */ Arms /*
-    <line x1="140" y1="120" x2="120" y2="100" className="figure-part" />
-    <line x1="140" y1="120" x2="160" y2="100" className="figure-part" />
+    {errors > 2 && <line x1="140" y1="120" x2="120" y2="100" className="figure-part" />}
+    {errors > 3 && <line x1="140" y1="120" x2="160" y2="100" className="figure-part" />}
     */ Legs /*
-    <line x1="140" y1="150" x2="120" y2="180" className="figure-part" />
-    <line x1="140" y1="150" x2="160" y2="180" className="figure-part" />
+    {errors > 4 && <line x1="140" y1="150" x2="120" y2="180" className="figure-part" />}
+    {errors > 5 && <line x1="140" y1="150" x2="160" y2="180" className="figure-part" />}
   </svg>
   )
 }
