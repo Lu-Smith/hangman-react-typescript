93302f698657a0de502b317629e8b846161c2d22
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-09 00:03:41 +0000

    updated helpers.ts

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -4,3 +4,15 @@ export function showNotification(setter: any) {
         setter(false);
     }, 2000);
 }
+
+export function checkWin(correct: string, wrong: string, word: string) {
+    let status = 'win';
+
+    //check for win
+
+    word.split('').forEach(letter => {
+        if(correct.includes(letter)){
+            status = '';
+        }
+    });
+}
